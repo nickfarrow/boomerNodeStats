26f0d0fc42db9b711f1678a33d9f910fc2a91c4a
nickfarrow <nicholas.w.farrow@gmail.com>
2020-09-07 16:26:28 +1000

    loop python file

--- update_html.py
+++ update_html.py
@@ -1,27 +1,43 @@
 #!/usr/bin/python
 from bitcoinrpc.authproxy import AuthServiceProxy
 import time
+import datetime
 import RPC_PASSWORDS as rp
+import subprocess
 
-rpc_connection = AuthServiceProxy("http://{}:{}@127.0.0.1:8332".format(rp.username,
-                                                               rp.password
-                                                               ))
-info = rpc_connection.getblockchaininfo()
+while True:
 
-print(info)
+    rpc_connection = AuthServiceProxy("http://{}:{}@127.0.0.1:8332".format(rp.username,
+                                                                           rp.password
+                                                                           ))
+    info = rpc_connection.getblockchaininfo()
 
-with open("index.html", 'r') as f:
-    siteContents = f.read()
+    print(info)
 
-preContents, middle, postContents = siteContents.split('<!---splithere-->')
+    with open("index.html", 'r') as f:
+        siteContents = f.read()
 
-middle = ""
+    preContents, middle, postContents = siteContents.split('<!---splithere-->\n')
 
-for infokey in ["blocks", "difficulty", "headers", "bestblockhash", "size_on_disk"]:
-    statValue = info[infokey]
-    middle += "<p><b>" + infokey + ": </b>" + str(statValue) + "</p>\n"
+    middle = ""
+
+    for infokey in ["blocks", "difficulty", "headers", "bestblockhash", "size_on_disk"]:
+        statValue = info[infokey]
+        middle += "<p><b>" + infokey + ": </b>" + str(statValue) + "</p>\n"
+    
+    now = datetime.datetime.now()
+    middle += "<p><b>Last Updated<b>: " + now.strftime("%Y-%m-%d %H:%M:%S") + " AEST</p>\n"
 
 
-splitLine = '<!---splithere-->\n'
-with open("index.html", 'w') as f:
-    f.write(preContents + splitLine + middle + splitLine + postContents)
+    splitLine = '<!---splithere-->\n'
+    with open("index.html", 'w') as f:
+        f.write(preContents + splitLine + middle + splitLine + postContents)
+
+    
+    try:
+        subprocess.call(["rsync", "-a", "./index.html", "root@202.182.97.180:/var/www/nodeStats/index.html"])
+    except Exception as e:
+        print("Error: " + e)
+
+    print("sleeping")
+    time.sleep(60)
